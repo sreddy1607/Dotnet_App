2eeda89dc516355f898935d252b0c918a9167407
sreddy1607 <151578951+sreddy1607@users.noreply.github.com>
2026-04-27 15:49:18 -0700

    Integrate deployment scripts into constants.py
    Added deployment scripts and environment configuration to constants.py.

--- constants.py
+++ constants.py
@@ -1,5 +1,27 @@
-CI_URL = ""
-LOGIN_URL = "/api/login"
-LOGOUT_URL = "/api/logout"
-GRAPH_URL = "/api/graphql"
-X_AUTH_TOKEN = ""
+@echo off
+
+REM Remove the files necessary to get apps pushed if they are empty
+DEL E:\tar-surge-Api-staging\Apiservices\placeholder.txt
+
+REM These values are replaced during the build based on parameters set
+SET "DEPLOY_FILES={DEPLOY_FILES}"
+SET "DEPLOY_CONFIG={DEPLOY_CONFIG}" 
+SET "DEPLOY_ENVIRONMENT={DEPLOY_ENVIRONMENT}"
+
+REM If environment is being pushed, perform thes first
+if %DEPLOY_ENVIRONMENT%==true (
+    echo "ENVIRONMENT will be deployed"
+    C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -NoProfile E:\tar-surge-Api-staging\environment\deploy-environment.ps1
+)
+
+REM If config is being pushed for the environment, perform these steps second
+if %DEPLOY_CONFIG%==true (
+    echo "CONFIG will be deployed"
+    C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -NoProfile E:\tar-surge-Api-staging\serverconfig\deploy-config.ps1
+)
+
+REM Finally, if files are being pushed, perform these steps last
+if %DEPLOY_FILES%==true (
+    echo "FILES will be deployed"
+    C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -NoProfile E:\tar-surge-Api-staging\scripts\deploy-files.ps1
+)
