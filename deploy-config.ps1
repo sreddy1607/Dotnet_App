7acd7101239e22bdced82112b4a8ce3a7fafe535
sreddy1607 <151578951+sreddy1607@users.noreply.github.com>
2026-04-28 09:22:27 -0700

    Update deploy-config.ps1 for Vault and Datadog
    Removed IIS installation commands and added environment variable settings for Vault and Datadog logging.

--- deploy-config.ps1
+++ deploy-config.ps1
@@ -1,15 +1,17 @@
-REM Install Internet Information Server (IIS). 
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -NoProfile -Command Import-Module -Name ServerManager
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -NoProfile -Command Install-WindowsFeature Web-Server
+# Set environment variables for Vault access
+Write-Host "Setting environment variables for Vault access"
+[Environment]::SetEnvironmentVariable("VAULT_ADDRESS", "{VAULT_ADDR}", "Machine")
+[Environment]::SetEnvironmentVariable("VAULT_APPROLE_ROLE_ID", "{APPROLE_ROLE_ID}", "Machine")
+[Environment]::SetEnvironmentVariable("VAULT_APPROLE_SECRET_ID", "{APPROLE_SECRET_ID}", "Machine")
+[Environment]::SetEnvironmentVariable("VAULT_SECRET_PATH", "{VAULT_SECRET_PATH}", "Machine")
+[Environment]::SetEnvironmentVariable("VAULT_SECRET_PATH_LTAR", "{VAULT_SECRET_PATH_LTAR}", "Machine")
+[Environment]::SetEnvironmentVariable("VAULT_SECRET_PATH_IMGVWR", "{VAULT_SECRET_PATH_IMGVWR}", "Machine")
+[Environment]::SetEnvironmentVariable("VAULT_APPROLE_AUTH_PATH", "{VAULT_APPROLE_AUTH_PATH}", "Machine")
 
-REM Backup existing configuration for IIS
-C:\Windows\System32\inetsrv\appcmd.exe add backup
+[Environment]::SetEnvironmentVariable("SURGE_ENVNAME", "{SURGE_ENVNAME}", "Machine")
+[Environment]::SetEnvironmentVariable("SURGE_RPM_ROOT", "{SURGE_RPM_ROOT}", "Machine")
+[Environment]::SetEnvironmentVariable("SURGE_RPM_ONLINE_KEY", "/online", "Machine")
 
-REM Ensure that api directory is empty before deploying files
-DEL /S /Q E:\tar-surge-Api-staging\Apiservices\*
 
-DEL /S /Q E:\tar-surge-Api-staging\serverconfig\*
-DEL /S /Q E:\tar-surge-Api-staging\environment\*
-DEL /S /Q E:\tar-surge-Api-staging\scripts\*
-
-exit 0
+Write-Host "Setting environment to enable Datadog file logging"
+[Environment]::SetEnvironmentVariable("DD_LOGS_ENABLED", "true", "Machine")
