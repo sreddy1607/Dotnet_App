78e5113a08ca28c7095e2a4dcd9497b4bce15470
sreddy1607 <151578951+sreddy1607@users.noreply.github.com>
2026-04-30 09:55:51 -0700

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -1,39 +1,15 @@
-import requests
-import constants
-import queries
-import time
+REM Install Internet Information Server (IIS). 
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -NoProfile -Command Import-Module -Name ServerManager
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -NoProfile -Command Install-WindowsFeature Web-Server
 
-# Mutation for promoting a label given target_instance_id and label_version_id.
-# Calls deployment monitoring to prompt user about deployment progress.
+REM Backup existing configuration for IIS
+C:\Windows\System32\inetsrv\appcmd.exe add backup
 
-def deploy(source_label_input, source_instance_id, deployment_target_input):
+REM Ensure that api directory is empty before deploying files
+DEL /S /Q E:\tar-surge-Api-staging\Apiservices\*
 
-    variables = {'input':{'source': {'instanceId': source_instance_id, 'label': source_label_input},
-                          'target':deployment_target_input}}
+DEL /S /Q E:\tar-surge-Api-staging\serverconfig\*
+DEL /S /Q E:\tar-surge-Api-staging\environment\*
+DEL /S /Q E:\tar-surge-Api-staging\scripts\*
 
-    response = requests.post(constants.GRAPH_URL, headers={'x-auth-token': constants.X_AUTH_TOKEN},
-                             json={'query': queries.DEPLOY_LABEL, 'variables': variables})
-    print_deployment_response(response.json())
-
-# Calls promotion monitoring to prompt user about promotion progress.
-def print_deployment_response(response_json):
-    if "errors" in response_json:
-        print("Error performing the deployment:\n ", response_json["errors"])
-    else:
-        deployment_id = response_json["data"]["deploy"]["deployment"]["id"]
-        print("deploymentID: " + str(deployment_id))
-        deployment_monitoring(deployment_id)
-        print("Deployed!")
-
-
-# Prompts user about promotion progress.
-def deployment_monitoring(deployment_id):
-    deployment_status = ""
-    while deployment_status != "DISALLOWED" and deployment_status != "EXECUTED":
-        variables = {'id': deployment_id}
-        response = requests.post(constants.GRAPH_URL, headers={'x-auth-token': constants.X_AUTH_TOKEN},
-                                 json={'query': queries.GET_DEPLOYMENT_STATE, 'variables': variables})
-        deployment_status = str(response.json()["data"]["deployment"]["state"])
-        print("deployment result: " + deployment_status)
-        time.sleep(6)
-    return
+exit 0
